62525a6d73e7546dbbabbdb538276e644bad3c14
Yovanmost <yovanmost@gmai.com>
2024-04-02 23:17:22 +0700

    History for render

---
+++ Info.py
@@ -0,0 +1,13 @@
+class GameInfo:
+    def init(self, seeker_position, hider_position, announcement_positions):
+        self.seeker_position = seeker_position
+        self.hider_position = hider_position
+        self.announcement_positions = announcement_positions
+
+    def printInfomationPos(self):
+        print ("Seeker Position:")
+        print(self.seeker_position)
+        print ("Hider Position:")
+        print(self.hider_position)
+        print ("Announcement Position:")
+        print(self.announcement_positions)
