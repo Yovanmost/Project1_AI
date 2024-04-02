114ca2faa323ba244e3617251e6384acd052643c
KhoaUs <128683739+KhoaUs@users.noreply.github.com>
2024-04-02 17:34:48 +0700

    Add files via upload

---
+++ Seeker.py
@@ -0,0 +1,115 @@
+from Agent import Agent
+
+WALL = 1
+
+class Seeker(Agent):
+    def vision(self, grid):
+        ls_vision = set()  # Initialize the vision list
+
+        x, y = self.position
+        size = (len(grid), len(grid[0]))
+
+        if (grid[x][y] == Agent.WALL):
+            return list(ls_vision)
+
+        flatten = [
+            [(-1,0), (-2,0), (-3,0)], # vertical
+            [(0,1), (0,2), (0,3)], # horizontal
+            [(-1,1), (-2,2), (-3,3)], # diagonal
+            [(-2,1), (-3,1), (-3,2)], # VxD
+            [(-1,2), (-1,3), (-2,3)] # HxD
+        ]
+
+        quarters = [
+            (1,1),   # quarter 1
+            (1,-1),  # quarter 2
+            (-1,-1), # quarter 3
+            (-1,1)   # quarter 4
+        ]
+        for quarter in quarters:
+            # Make new flatten for each quarter
+            new_flatten = []
+            for sublist in flatten:
+                new_sublist = []
+                for point in sublist:
+                    new_point = (quarter[0] * point[0], quarter[1] * point[1])
+                    new_sublist.append(new_point)
+                new_flatten.append(new_sublist)
+
+            # check vertical
+            flag_v = False
+            for i in range(3):
+                new_x, new_y = x + new_flatten[0][i][0], y + new_flatten[0][i][1]
+                if not (Agent.is_inside((new_x, new_y), size)):
+                    break
+                if (grid[new_x][new_y] == Agent.WALL):
+                    print(new_x, new_y)
+                    if (i == 0):
+                        flag_v = True
+                    del new_flatten[0][i+1:]
+                    break
+
+            # check horizontal
+            flag_h = False
+            for i in range(3):
+                new_x, new_y = x + new_flatten[1][i][0], y + new_flatten[1][i][1]
+                if not (Agent.is_inside((new_x, new_y), size)):
+                    break
+                if (grid[new_x][new_y] == Agent.WALL):
+                    if (i == 0):
+                        flag_h = True
+                    del new_flatten[1][i+1:]
+                    break
+
+            # check diagonal
+            flag_d = False
+            for i in range(3):
+                new_x, new_y = x + new_flatten[2][i][0], y + new_flatten[2][i][1]
+                if not (Agent.is_inside((new_x, new_y), size)):
+                    break
+                if (grid[new_x][new_y] == Agent.WALL):
+                    if (i == 0):
+                        flag_d = True
+                    del new_flatten[2][i+1:]
+                    break
+
+            if flag_d:
+                del new_flatten[3][2]
+                del new_flatten[4][2]
+
+            if flag_v:
+                del new_flatten[3][1]
+
+            if flag_h:
+                del new_flatten[4][1]
+
+            if flag_d and flag_v:
+                del new_flatten[3][:]
+
+            if flag_d and flag_h:
+                del new_flatten[4][:]
+
+            # check VxD
+            if (len(new_flatten[3]) > 0):
+                new_x, new_y = x + new_flatten[3][0][0], y + new_flatten[3][0][1]
+                if (Agent.is_inside((new_x, new_y), size)):
+                    if grid[new_x][new_y] == Agent.WALL:
+                        del new_flatten[3][1:]
+            # check HxD
+            if (len(new_flatten[4]) > 0):
+                new_x, new_y = x + new_flatten[4][0][0], y + new_flatten[4][0][1]
+                if (Agent.is_inside((new_x, new_y), size)):
+                    if grid[new_x][new_y] == Agent.WALL:
+                        del new_flatten[4][1:]
+
+            ls_vision.add((x, y))
+
+            for sublist in new_flatten:
+                for point in sublist:
+                    new_x = point[0] + x
+                    new_y = point[1] + y
+                    if not Agent.is_inside((new_x, new_y), size) or grid[new_x][new_y] == WALL:
+                        continue
+                    ls_vision.add((new_x, new_y))
+
+        return list(ls_vision)
--- board.py
+++ board.py
@@ -1,20 +1,13 @@
-import random
+from Agent import Agent
+from Seeker import Seeker
+from Hider import Hider
 
 # CONSTANT
 WALL = 1
 HIDER = 2
 SEEKER = 3
 PATH = 0
-
-class Hider:
-    def __init__(self, pos):
-        self.pos = pos
-
-    def announcePos(self):
-        # scan the whole 3x3 surrounding
-        # add all free space into a list for random
-        # pick one and send a signal(?)
-        pass
+VISION = 5
 
 class Cell:
     def __init__(self, pos, state, value):
@@ -23,28 +16,38 @@ class Cell:
         self.value = value
 
 class Board:
-    def __init__(self, size, grid, obstacle):
+    def __init__(self, size, grid, obstacles):
         self.size = size # size[0] = N, size[1] = M
         self.grid = grid # NxM matrix
-        self.obstacle = obstacle # list of (upper left, bottom right) pos
+        self.obstacles = obstacles # list of (upper left, bottom right) pos
+        self.hider = self.findHider()
+        self.seeker = self.findSeeker()
 
-    # find pos hider
+    # init hider
     def findHider(self):
         hiderPos = 0,0
+        listHider = []
         for i in range(self.size[0]):
             for j in range(self.size[1]):
                 if self.grid[i][j] == HIDER:
                     hiderPos = i, j
-        return hiderPos
+                    listHider.append(Hider(hiderPos))
+        return listHider
 
-    # find pos seeker
+    # init seeker
     def findSeeker(self):
         seekerPos = 0,0
         for i in range(self.size[0]):
             for j in range(self.size[1]):
                 if self.grid[i][j] == SEEKER:
                     seekerPos = i, j
-        return seekerPos
+        seeker = Seeker(seekerPos)
+        return seeker
+
+    def isFound(self):
+        # newest pos comparison
+        return self.seeker.position == self.hider.position
+
 
     def printBroad(self):
         for i in range(self.size[0]):
@@ -53,13 +56,18 @@ class Board:
                     print()
                 print(self.grid[i][j], end=" ")
 
-testSize = 3, 3
-testGrid = [[3, 0, 0],
-            [0, 0, 1],
-            [2, 0, 0]]
-testObstacle = [((1, 0), (1, 0))]
-testBoard = Board(testSize, testGrid, testObstacle)
-testBoard.printBroad()
-print()
-print(testBoard.findHider())
-print(testBoard.findSeeker())
+    # update board based on seeker/hider vision(?)
+    def updateBoard(self, pos, listVision):
+        # print(pos)
+        newGrid = self.grid.copy()
+        for k in range(len(listVision)):
+            if not (Agent.is_inside((pos[0] + listVision[k][0], pos[1] + listVision[k][1]), self.size)):
+                continue
+            if newGrid[pos[0] + listVision[k][0]][pos[1] + listVision[k][1]] == 1:
+                continue
+            newGrid[pos[0] + listVision[k][0]][pos[1] + listVision[k][1]] = 5
+        return newGrid
+
+    def addAnnounce(self):
+        pos = self.hider.announce(self.grid)
+        self.grid[pos[0]][pos[1]] = 6
